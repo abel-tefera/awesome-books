9f7b5c91a2e09db210c4e40ab6c9f6e3f4330543
abel-tefera <abeltefera16@gmail.com>
2023-03-21 10:48:15 +0300

    Books refactored into classes

--- js/index.js
+++ js/index.js
@@ -8,17 +8,66 @@ class Book {
     this.id = Date.now();
     this.title = title;
     this.author = author;
-    this.description = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+    this.description = `Lorem ipsum dolor sit amet 
+    consectetur adipisicing elit. Velit
     earum saepe fugiat!`;
     this.cover = `assets/book.png`;
   }
-
-  addBook(book) {
-
-  }
-
-  removeBook() {}
 }
+
+class Library {
+  constructor() {
+    const booksDataFromLocalStorage = JSON.parse(
+        localStorage.getItem('booksData'),
+    );
+    const booksLSExists =
+      booksDataFromLocalStorage !== undefined &&
+      booksDataFromLocalStorage !== null;
+
+    this.booksData = booksLSExists ?
+      booksDataFromLocalStorage :
+      [
+        {
+          id: 1,
+          title: `Lorem, ipsum.`,
+          author: `John Doe`,
+          description: `Lorem ipsum dolor sit amet consectetur 
+          adipisicing elit. Velit
+            earum saepe fugiat!`,
+          cover: `assets/book.png`,
+        },
+        {
+          id: 2,
+          title: `Lorem, ipsum.`,
+          author: `John Doe`,
+          description: `Lorem ipsum dolor sit amet consectetur 
+          adipisicing elit. Velit
+            earum saepe fugiat!`,
+          cover: `assets/book.png`,
+        },
+        {
+          id: 3,
+          title: `Lorem, ipsum.`,
+          author: `John Doe`,
+          description: `Lorem ipsum dolor sit amet consectetur 
+          adipisicing elit. Velit
+            earum saepe fugiat!`,
+          cover: `assets/book.png`,
+        },
+        {
+          id: 4,
+          title: `Lorem, ipsum.`,
+          author: `John Doe`,
+          description: `Lorem ipsum dolor sit amet consectetur 
+          adipisicing elit. Velit
+            earum saepe fugiat!`,
+          cover: `assets/book.png`,
+        },
+      ];
+  }
+}
+
+const library = new Library();
 
 class bookCard extends HTMLElement {
   constructor() {
@@ -26,7 +75,7 @@ class bookCard extends HTMLElement {
   }
 
   connectedCallback() {
-    const { id, title, author, description, cover } = this.attributes;
+    const {id, title, author, description, cover} = this.attributes;
 
     this.innerHTML = `
         <div class="card mx-auto">
@@ -43,35 +92,33 @@ class bookCard extends HTMLElement {
         </div>
       `;
 
-    const cardBtns = document.querySelectorAll(".card-btn");
+    const cardBtns = document.querySelectorAll('.card-btn');
     cardBtns.forEach((cardBtn) => {
-      const idx = parseInt(cardBtn.id.split("-")[2]);
-      cardBtn.addEventListener("click", () => {
-        booksData = booksData.filter(({ id }) => id !== idx);
-        localStorage.setItem("booksData", JSON.stringify(booksData));
-        recreateUI();
+      const idx = parseInt(cardBtn.id.split('-')[2]);
+      cardBtn.addEventListener('click', () => {
+        removeBook(idx);
       });
     });
   }
 }
 
-customElements.define("book-card", bookCard);
+customElements.define('book-card', bookCard);
 
-const addBookModal = document.querySelector(".modal");
+const addBookModal = document.querySelector('.modal');
 
-const recreateUI = (currentBooksData = booksData) => {
-  const existingBooks = document.querySelector(".books-container");
+const recreateUI = () => {
+  const existingBooks = document.querySelector('.books-container');
   if (existingBooks) {
     existingBooks.remove();
   }
-  const mainContainer = document.querySelector(".main-container");
-  const bookContainer = document.createElement("div");
-  bookContainer.classList.add("row", "books-container");
+  const mainContainer = document.querySelector('.main-container');
+  const bookContainer = document.createElement('div');
+  bookContainer.classList.add('row', 'books-container');
 
-  for (const book of currentBooksData) {
-    const { id, title, author, description, cover } = book;
-    const bookItem = document.createElement("div");
-    bookItem.classList.add("col-12", "col-md-6", "col-lg-4", "my-2");
+  for (const book of library.booksData) {
+    const {id, title, author, description, cover} = book;
+    const bookItem = document.createElement('div');
+    bookItem.classList.add('col-12', 'col-md-6', 'col-lg-4', 'my-3');
     bookItem.innerHTML = `<book-card
         id=${JSON.stringify(id)}
         title=${JSON.stringify(title)}
@@ -87,42 +134,24 @@ const recreateUI = (currentBooksData = booksData) => {
 
 const addBookHandler = (e) => {
   e.preventDefault();
-  const bookTitle = document.querySelector(".new-book-title").value;
-  const bookAuthor = document.querySelector(".new-book-author").value;
+  const bookTitle = document.querySelector('.new-book-title').value;
+  const bookAuthor = document.querySelector('.new-book-author').value;
 
-  booksData.push({
-    id: Date.now(),
-    title: bookTitle,
-    author: bookAuthor,
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-        earum saepe fugiat!`,
-    cover: "assets/book.png",
-  });
+  const book = new Book(bookTitle, bookAuthor);
+  library.addBook(book);
+  recreateUI();
+};
 
-  localStorage.setItem("booksData", JSON.stringify(booksData));
+const removeBook = (idx) => {
+  library.removeBook(idx);
   recreateUI();
 };
 
 const main = () => {
-  const booksDataFromLocalStorage = JSON.parse(
-    localStorage.getItem("booksData")
-  );
-  const booksLSExists =
-    booksDataFromLocalStorage !== undefined &&
-    booksDataFromLocalStorage !== null;
-
-  const initialBooksData = booksLSExists
-    ? booksDataFromLocalStorage
-    : booksData;
-
-  if (booksLSExists) {
-    booksData = booksDataFromLocalStorage;
-  }
-
-  recreateUI(initialBooksData);
+  recreateUI();
 };
 
 main();
 
-const form = document.querySelector(".add-book-form");
-form.addEventListener("submit", addBookHandler);
+const form = document.querySelector('.add-book-form');
+form.addEventListener('submit', addBookHandler);
